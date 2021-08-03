482b55c0dfc273f976ce70ebe88f2a635a6b8c17
zel <188508014@qq.com>
2021-08-03 21:07:40 +0800

    update JobMain

--- mr-day02-1/src/main/java/com/nx/partitioner/JobMain.java
+++ mr-day02-1/src/main/java/com/nx/partitioner/JobMain.java
@@ -1,8 +1,13 @@
 package com.nx.partitioner;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.retry.LossyRetryInvocationHandler;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
@@ -13,13 +18,36 @@ import java.io.IOException;
  * @Modified By:
  */
 public class JobMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         //一.初始化一个Job
-        Configuration conf = new Configuration;
+        Configuration conf = new Configuration();
         Job job = Job.getInstance(conf,"partitioner");
 
         //二.设置Job的相关信息 8个小步骤
         //1.设置输入路径
-        job.setInputFormatClass();
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job,new Path("D://zel/MrTest/WordCount/test1.txt"));
+        //2.设置Mapper类型，并设置k2,v2
+        job.setMapperClass(WordMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        //3 4 5 6Shuffle阶段
+        //3.设置分区
+        job.setPartitionerClass(Mypartitioner.class);
+        //7.设置Reduce类型
+        job.setReducerClass(WordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        //设置ReduceTask的个数
+        job.setNumReduceTasks(2);
+        //8.设置输出路径
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job,new Path("D://zel/MrTest/WordCount/word_out"));
+        //三、等待程序完成（任务的提交）
+        boolean b = job.waitForCompletion(true);
+        System.out.println(b);
+        System.exit(b ? 0 : 1);
     }
 }
